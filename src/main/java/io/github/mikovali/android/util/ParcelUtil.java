f5360517e1b0b55fbaddbeac7fb15248a7ce7759
Miko Väli <mikovali@gmail.com>
2015-02-05 22:35:36 +0200

    Helpers in ParcelUtil

--- src/main/java/io/github/mikovali/android/util/ParcelUtil.java
+++ src/main/java/io/github/mikovali/android/util/ParcelUtil.java
@@ -11,28 +11,34 @@ public final class ParcelUtil {
     private static final int NOT_NULL = 0;
 
     public static void writeInteger(Integer value, Parcel dest) {
-        if (value == null) {
-            dest.writeInt(NULL);
-        } else {
-            dest.writeInt(NOT_NULL);
+        if (!setNull(value, dest)) {
             dest.writeInt(value);
         }
     }
 
     public static Integer readInteger(Parcel in) {
-        return in.readInt() == NULL ? null : in.readInt();
+        return isNull(in) ? null : in.readInt();
     }
 
     public static void writeLong(Long value, Parcel dest) {
-        if (value == null) {
-            dest.writeInt(NULL);
-        } else {
-            dest.writeInt(NOT_NULL);
+        if (!setNull(value, dest)) {
             dest.writeLong(value);
         }
     }
 
     public static Long readLong(Parcel in) {
-        return in.readInt() == NULL ? null : in.readLong();
+        return isNull(in) ? null : in.readLong();
+    }
+
+    // helpers
+
+    private static boolean setNull(Object value, Parcel dest) {
+        final boolean isNull = value == null;
+        dest.writeInt(isNull ? NULL : NOT_NULL);
+        return isNull;
+    }
+
+    private static boolean isNull(Parcel in) {
+        return in.readInt() == NULL;
     }
 }
